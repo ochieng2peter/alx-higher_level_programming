fce5b5562a2441bb3d204feae6e16eee5d8a7033
ochieng2peter <ochieng2.peter@gmail.com>
2023-05-11 02:39:26 +0300

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19 \n")
+
+# Write the desired message to stderr
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exit with status code 1
 sys.exit(1)
