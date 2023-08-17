1df3761ef74db16810ea76a27473a82a6fd53eb9
ochieng2peter <ochieng2.peter@gmail.com>
2023-08-18 00:15:21 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,11 @@
 -- Creates the user user_0d_1 with all privileges.
+-- Check if user exists
+SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost';
+-- Apply privileges immediately
+FLUSH PRIVILEGES;
